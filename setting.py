3cb873d107c6aad219bf3dc5ed831c3277db86e2
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-04 21:57:05 +0100

    creating seetings file

---
+++ setting.py
@@ -0,0 +1,8 @@
+class Settings():
+    '''CLass for game settings'''
+
+    def __init__(self):
+        '''Inizalizing game setings'''
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
