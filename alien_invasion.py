d37fb35840dffbbdb966b2489c9a604d89c5fb6b
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-04 22:04:13 +0100

    rename

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,14 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     # Initializing the game and making screen object.
     pygame.init()
+    ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    ai_settings = Settings()
     pygame.display.set_caption('Space invazion!')
     # Background color
     bg_color = (230, 230, 230)
