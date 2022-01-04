4e937a69dbb18ca0b761d2b68168dde00a9ed78c
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-04 20:38:20 +0100

    creating file content

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,22 @@
+import sys
 
+import pygame
+
+def run_game():
+    # Initializing the game and making screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('Space invazion!')
+
+    # Main loop start
+    while True:
+
+        # Waiting for keybord or mouse click
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    # Displaing last modyfid screen
+    pygame.display.flip()
+
+run_game()        
