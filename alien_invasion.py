b7a6a7e9489273d57d556da7db0a5a08d898740e
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-05 15:42:46 +0100

    refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initializing the game and making screen object.
@@ -18,11 +17,7 @@ def run_game():
     bg_color = (230, 230, 230)
     # Main loop start
     while True:
-
-        # Waiting for keybord or mouse click
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
     
         # Refreshing screen in every iteration of loop
         screen.fill(ai_settings.bg_color)
