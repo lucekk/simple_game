3a9f8e6a0d7d3c0a3b8d98560d74cb55eefdda6e
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-14 14:00:14 +0100

    edit

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption('Space invazion!')
+    pygame.display.set_caption('BAGNO invazion!')
     # Making space ship
     ship = Ship(ai_settings, screen)
     # Group to contein bullets
