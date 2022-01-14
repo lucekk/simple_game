13d119d120f33314a87d878316ff3dcb658d4568
lucekk <30288880+lucekk@users.noreply.github.com>
2022-01-14 14:01:25 +0100

    rename game

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption('BAGNO invazion!')
+    pygame.display.set_caption('Bagno invazion!')
     # Making space ship
     ship = Ship(ai_settings, screen)
     # Group to contein bullets
